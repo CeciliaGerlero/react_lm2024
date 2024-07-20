37d6a87da3ee11d5b58bdc7149d0978c668cbd7d
Cecilia Gerlero <mcgerlero@gmail.com>
2024-07-20 12:12:38 -0300

    fin clse 26

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = (props)=>{
+    return(
+        <p>Diseñado por: MCG - 2024</p>
+    )}
+    export default Footer;
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+
+import { Link } from "react-router-dom";
+
+const Nav = (props) => {
+    return (
+        // <h1> Hola, soy el navegador</h1>
+         <nav>
+            <ul className="holder">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/nosotros">Nosotros</Link></li>
+                <li><Link to="/servicios">Servicios</Link></li>
+                <li><Link to="/contacto">Contacto</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
